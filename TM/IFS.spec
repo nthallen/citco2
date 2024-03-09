ff6b74af802220727276d569585276cf59445321
Norton Allen <allen@huarp.harvard.edu>
2024-03-08 20:49:59 -0500

    Add Modules Email and savelog

--- TM/IFS.spec
+++ TM/IFS.spec
@@ -1,6 +1,8 @@
 # IFS.spec
 tmcbase = base.tmc
 Module tmbase mode=ignore
+Module savelog
+Module Email dest=/home/citco2/bin/Email
 
 TGTDIR = /home/citco2
 SCRIPT = VERSION Experiment.config
---
+++ TM/doreport.pm
@@ -0,0 +1,11 @@
+package DoReport;
+
+our %default_msg = (
+  To => [ 'Norton Allen <allen@huarp.harvard.edu>' ],
+  From => 'MOACC_ifs13 <tccon-moacc@huarp.harvard.edu>',
+  Subject => 'MOACC_ifs13',
+  Message => 'stuff',
+  Message_ID_Template => 'moacc-ifs13-%d-%d-%d@huarp.harvard.edu'
+);
+
+1;
