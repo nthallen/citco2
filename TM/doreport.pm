8bac3882eed63975259d6e8eec71a8ba041ff69e
Norton Allen <allen@huarp.harvard.edu>
2024-10-17 11:48:02 -0400

    New emails

--- TM/doreport.pm
+++ TM/doreport.pm
@@ -1,7 +1,12 @@
 package DoReport;
 
 our %default_msg = (
-  To => [ 'Norton Allen <allen@huarp.harvard.edu>' ],
+  To => [ 'Norton Allen <allen@huarp.harvard.edu>',
+          'Orfeo Colebatch <orfeo.colebatch@utoronto.ca>',
+          'Kim Strong <strong@atmosp.physics.utoronto.ca>',
+          'Erin McGee <e.mcgee@mail.utoronto.ca>',
+          'jblavier <jblavier@caesar.jpl.nasa.gov>'
+        ],
   From => 'MOACC_ifs13 <tccon-moacc@huarp.harvard.edu>',
   Subject => 'MOACC_ifs13',
   Message => 'stuff',
