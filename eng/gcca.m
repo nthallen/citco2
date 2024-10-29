79691f5618314ef0cbd1d741fa97e01558f4f5ee
Norton Allen <allen@huarp.harvard.edu>
2024-10-28 20:48:20 -0400

    V3.0.7 RH_Open/Close_Pct Algorithm current state display

---
+++ eng/gcca.m
@@ -0,0 +1,5 @@
+function fig = gcca(varargin);
+% gcca(...)
+% Algo
+ffig = ne_group(varargin,'Algo','pccap','pccap3');
+if nargout > 0 fig = ffig; end
--- eng/gccwtx.m
+++ eng/gccwtx.m
@@ -1,5 +1,5 @@
 function fig = gccwtx(varargin);
 % gccwtx(...)
 % WTX
-ffig = ne_group(varargin,'WTX','pccwtxt','pccwtxrh','pccwtxp','pccwtxsr');
+ffig = ne_group(varargin,'WTX','pccwtxrh','pccwtxt','pccwtxp','pccwtxsr');
 if nargout > 0 fig = ffig; end
