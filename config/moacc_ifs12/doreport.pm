efd6a7325a6c0250be27541c1ca97f6d45aa7f77
Norton Allen <allen@huarp.harvard.edu>
2024-11-08 10:33:36 -0500

    Switch from _ifs13 to _ifs12

--- config/moacc_ifs12/doreport.pm
+++ config/moacc_ifs12/doreport.pm
@@ -7,8 +7,8 @@ our %default_msg = (
           'Erin McGee <e.mcgee@mail.utoronto.ca>',
           'jblavier <jblavier@caesar.jpl.nasa.gov>'
         ],
-  From => 'MOACC_ifs13 <tccon.cambridgebay@huarp.harvard.edu>',
-  Subject => 'MOACC_ifs13',
+  From => 'moacc_ifs12 <tccon.cambridgebay@huarp.harvard.edu>',
+  Subject => 'moacc_ifs12',
   Message => 'stuff',
   Message_ID_Template => 'moacc-ifs13-%d-%d-%d@huarp.harvard.edu'
 );
