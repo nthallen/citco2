4c061e1947e226f88a9c880400cced642f4575fc
Norton Allen <allen@huarp.harvard.edu>
2025-09-28 14:16:46 -0400

    infoext (3.1.2): Deal with WTX non-numbers

--- eng/pccwtxwstatus.m
+++ eng/pccwtxwstatus.m
@@ -3,4 +3,4 @@ function pccwtxwstatus(varargin)
 % WTX Wind Status
 h = ne_dstat({
   'is_high', 'ws_high', 0; ...
-	'is_log', 'ws_low', 0 }, 'Status', varargin{:} );
+	'is_low', 'ws_low', 0 }, 'Status', varargin{:} );
